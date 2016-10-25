13b2926f76d571d1eea8670f5f6f2abb45012fd1
JacobAllen218 <02jallen@gmail.com>
2016-10-25 18:00:35 -0400

    Base Commit

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@Autonomous(name = "Basic BasicAuto", group = "Red")
+public class BasicAuto extends OpMode{
+
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+
+    @Override
+    public void init() {
+
+        leftDrive = hardwareMap.dcMotor.get("left");
+        rightDrive = hardwareMap.dcMotor.get("right");
+
+    }
+
+    @Override
+    public void loop() {
+    }
+
+}
---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDrive.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Basic Drive")
+public class BasicDrive extends OpMode{
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
