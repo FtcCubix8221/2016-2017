2c5db67b81ad50a6bd2d2fa63c710ae2f5996eb2
JacobAllen218 <02jallen@gmail.com>
2016-10-25 17:20:50 -0700

    Updated Auto Program

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 
 @Autonomous(name = "Basic BasicAuto", group = "Red")
@@ -11,19 +12,29 @@ public class BasicAuto extends OpMode{
     DcMotor leftDrive;
     DcMotor rightDrive;
 
+    TouchSensor touch;
+
     @Override
     public void init() {
 
         leftDrive = hardwareMap.dcMotor.get("left");
         rightDrive = hardwareMap.dcMotor.get("right");
 
+        touch = hardwareMap.touchSensor.get("touch");
+
     }
 
     @Override
     public void loop() {
 
-        leftDrive.setPower(1);
-        rightDrive.setPower(1);
+        if(touch.isPressed()){
+            leftDrive.setPower(-1);
+            rightDrive.setPower(-1);
+        }else{
+            leftDrive.setPower(1);
+            rightDrive.setPower(1);
+        }
+
 
     }
 
